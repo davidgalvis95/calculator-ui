f1af8ea2059bb0b00db0513149b5bb8520bb4c8b
David Galvis <david.galvis@ymedialabs.com>
2023-07-25 17:52:07 -0500

    Adding linting rules

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./components/login/SignIn";
